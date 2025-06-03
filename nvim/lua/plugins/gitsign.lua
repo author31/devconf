ca4f8536d8384c9860d8d671df48c04ee362650d
author <author@authors-MacBook-Air.local>
2025-06-03 15:53:30 +0800

    feat: add diffview.lua

--- nvim/lua/plugins/gitsign.lua
+++ nvim/lua/plugins/gitsign.lua
@@ -19,19 +19,19 @@ return {
         untracked = { text = "┆" },
       },
       signs_staged_enable = true,
-      signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-      numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
-      linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
-      word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+      signcolumn = true,
+      numhl = false,
+      linehl = false,
+      word_diff = false,
       watch_gitdir = {
         follow_files = true,
       },
       auto_attach = true,
       attach_to_untracked = false,
-      current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame = false,
       current_line_blame_opts = {
         virt_text = true,
-        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        virt_text_pos = "eol",
         delay = 1000,
         ignore_whitespace = false,
         virt_text_priority = 100,
@@ -40,16 +40,57 @@ return {
       current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
       sign_priority = 6,
       update_debounce = 100,
-      status_formatter = nil, -- Use default
-      max_file_length = 40000, -- Disable if file is longer than this (in lines)
+      status_formatter = nil,
+      max_file_length = 40000,
       preview_config = {
-        -- Options passed to nvim_open_win
         border = "single",
         style = "minimal",
         relative = "cursor",
         row = 0,
         col = 1,
       },
+      -- Add the on_attach callback here
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+        
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Add your keymaps here
+        map("n", "]h", function()
+          if vim.wo.diff then
+            vim.cmd.normal({ "]c", bang = true })
+          else
+            gs.nav_hunk("next")
+          end
+        end, { desc = "Next Hunk" })
+        
+        map("n", "[h", function()
+          if vim.wo.diff then
+            vim.cmd.normal({ "[c", bang = true })
+          else
+            gs.nav_hunk("prev")
+          end
+        end, { desc = "Prev Hunk" })
+        
+        map("n", "]H", function() gs.nav_hunk("last") end, { desc = "Last Hunk" })
+        map("n", "[H", function() gs.nav_hunk("first") end, { desc = "First Hunk" })
+        map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", { desc = "Stage Hunk" })
+        map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", { desc = "Reset Hunk" })
+        map("n", "<leader>ghS", gs.stage_buffer, { desc = "Stage Buffer" })
+        map("n", "<leader>ghu", gs.undo_stage_hunk, { desc = "Undo Stage Hunk" })
+        map("n", "<leader>ghR", gs.reset_buffer, { desc = "Reset Buffer" })
+        map("n", "<leader>ghp", gs.preview_hunk_inline, { desc = "Preview Hunk Inline" })
+        map("n", "<leader>gb", function() gs.blame_line({ full = true }) end, { desc = "Blame Line" })
+        map("n", "<leader>gB", function() gs.blame() end, { desc = "Blame Buffer" })
+        map("n", "<leader>gd", gs.diffthis, { desc = "Diff This" })
+        map("n", "<leader>gD", function() gs.diffthis("~") end, { desc = "Diff This ~" })
+        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "GitSigns Select Hunk" })
+      end,
     }
   end,
 }
+
---
+++ nvim/lua/plugins/mini-animate.lua
@@ -0,0 +1,8 @@
+return { 
+  'echasnovski/mini.animate', 
+  version = '*',
+  config = function()
+    local animate = require("mini.animate")
+    animate.setup()
+    end
+}
